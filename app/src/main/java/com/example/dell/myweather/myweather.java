5028a9549f7d79f597baab4b1546150fb3429c71
unknown <>
2016-07-08 18:03:51 +0800

    first commit

---
+++ app/src/main/java/com/example/dell/myweather/myweather.java
@@ -0,0 +1,15 @@
+package com.example.dell.myweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dell on 2016/7/8.
+ */
+public class myweather extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
